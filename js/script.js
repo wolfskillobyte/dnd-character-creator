40389429660875618e9094da153071e1a6c85a4b
RobertAReedy <robertandreedy@gmail.com>
2021-10-12 12:51:48 -0400

    refactored API calling functionality and OH MY GOD LOOK HOW SMALL IT IS NOW I AM THE GOLDEN GOD OF PROGRAMMING

--- js/script.js
+++ js/script.js
@@ -6,18 +6,21 @@ var savedChars = [];
 var dropDownOptions = document.querySelector("#saved-chars").children;
 
 /**
- * Exists only to call getDndName(), which will call getClass(),
- * which will call getRace()... until all the gets are activated.
- * By doing all these calls from inside the fetch statements, it
- * is now guaranteed that each will occur in order, and by hiding the
- * Save Character button at the beginning of this process, and revealing
- * it at the end, we can guarantee no wonkiness from oddly timed
- * saving. However, this is an extremely inelegant and volatile 
- * solution that's difficult to work with. I want something better,
- * but I don't know how. 
+ * pls work god
  */
-function getNewCharacter() {
+async function getNewCharacter() {
     getDndName();
+    var requests = [];
+    requests.push(getTrait("races"));
+    requests.push(getTrait("classes"));
+    requests.push(getTrait("alignments"));
+
+    var results = await Promise.all(requests).catch(console.error);
+    setTrait("#char-race", results[0]);
+    setTrait("#char-class", results[1]);
+    setTrait("#char-align", results[2]);
+
+    changeImg();
 }
 
 /**
@@ -31,7 +34,6 @@ var getDndName = function() {
     var apiURL = "https://api.fungenerators.com/name/generate?category=shakespearean&limit=500&variation=any";
     
     charName.textContent = "";
-    $("#save-char").attr("class", "hide");
 
     if (!localStorage.getItem("names")) { //checks to see if the name array is already in localstorage
         console.log("Names not found in localstorage; calling database");
@@ -65,31 +67,7 @@ var getDndName = function() {
         charName.textContent = "Your character is " 
         + characterNames[Math.floor(Math.random() * characterNames.length)];
         console.log("getDndName complete");
-        getClass();
     }
-}
-
-/**
- * Randomly assigns a class. 
- */
-var getClass = function() {
-    var apiUrl = "https://www.dnd5eapi.co/api/classes";
-
-    fetch(apiUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        $("#char-class").text((response.results[Math.floor(Math.random() * response.results.length)].name));
-        changeImg();
-    })
-    .catch(function(error) {
-        console.log("Unable to reach class data");
-    })
-    .finally(function() {
-        console.log("getClass complete");
-        getRace();
-    });
 }
 
 // Concatenizes the "src" with the randomly generated class
@@ -98,48 +76,23 @@ function changeImg() {
 };
 
 /**
- * Randomly assigns a race. 
+ * Here we go...
  */
- var getRace = function() {
-    var apiUrl = "https://www.dnd5eapi.co/api/races";
-
-    fetch(apiUrl)
-    .then(function(response) {
-        return response.json();
+ var getTrait = function(trait) {
+    var apiUrl = "https://www.dnd5eapi.co/api/" + trait;
+    return new Promise(function(resolve, reject) {
+        fetch(apiUrl)
+        .then(function(response) {
+            return resolve(response.json())
+        })
+        .catch(reject);
     })
-    .then(function(response) {
-        $("#char-race").text((response.results[Math.floor(Math.random() * response.results.length)].name));
-    })
-    .catch(function(error) {
-        console.log("Unable to reach race data");
-    })
-    .finally(function() {
-        console.log("getRace complete");
-        getAlignment();
-    });
 }
 
-/**
- * Randomly assigns an alignment. 
- */
- var getAlignment = function() {
-    var apiUrl = "https://www.dnd5eapi.co/api/alignments";
-
-    fetch(apiUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        $("#char-align").text((response.results[Math.floor(Math.random() * response.results.length)].name));
-    })
-    .catch(function(error) {
-        console.log("Unable to reach alignment data");
-    })
-    .finally(function() {
-        console.log("getAlignment complete");
-        changeImg();
-        $("#save-char").attr("class", "");
-    });
+function setTrait(traitQueryString, promise) {
+    // var resolvedPromise = await promise;
+    var traitName = (promise.results[Math.floor(Math.random() * promise.results.length)].name);
+    $(traitQueryString).text(traitName);
 }
 
 /**
